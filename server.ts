2a56276df4744bfbdd4f121f639b176d588d20fd
navaltech3d <navaltech3d@gmail.com>
2026-03-04 11:04:51 -0300

    Remove commented-out server listen code

--- server.ts
+++ server.ts
@@ -769,9 +769,7 @@ async function startServer() {
 
 //  app.listen(PORT, '0.0.0.0', () => {
 //    console.log(`Server running on http://localhost:${PORT}`);
-  });
-}
-
+  
 if (!process.env.VERCEL) {
   startServer();
 }
